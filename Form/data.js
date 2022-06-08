5ac41a9d2cf5ffaa6da7bb882714fc3e029ccd2f
Arsham911git <99877942+Arsham911git@users.noreply.github.com>
2022-06-08 21:54:43 +1000

    Add files via upload

---
+++ Form/data.js
@@ -0,0 +1,18 @@
+const serverData = await fetch('http://localhost:3000/applications', {
+    method: 'GET',
+});
+const data = await serverData.json();
+data.forEach(participant => {
+    if (participant.hobbies == undefined) {
+        participant.hobbies = "нет";
+    }
+    const tableData = document.createElement('tr');
+    tableData.innerHTML = 
+        `<td>${participant.name}</td>
+        <td>${participant.email}</td>
+        <td>${participant.mobile}</td>
+        <td>${participant.country}</td>
+        <td>${participant.gender}</td>
+        <td>${participant.hobbies}</td>`
+    document.getElementById('table').appendChild(tableData);
+});
---
+++ Form/script.js
@@ -0,0 +1,87 @@
+function printError(elemId, hintMsg) {
+    document.getElementById(elemId).innerHTML = hintMsg;
+}
+function validateForm() {
+    let name = document.contactForm.name.value;
+    let email = document.contactForm.email.value;
+    let mobile = document.contactForm.mobile.value;
+    let country = document.contactForm.country.value;
+    let gender = document.contactForm.gender.value;
+    let hobbies = [];
+    let checkboxes = document.getElementsByName("hobbies");
+    for (let i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            hobbies.push(checkboxes[i].value);
+        }
+    }
+
+    let nameErr = emailErr = mobileErr = countryErr = genderErr = true;
+
+    if (name == "") {
+        printError("nameErr", "Введите имя");
+    } else {
+        let regex = /^[а-яa-z]+$/iu;
+        if (regex.test(name) === false) {
+            printError("nameErr", "Введите имя корректно");
+        } else {
+            printError("nameErr", "");
+            nameErr = false;
+        }
+    }
+
+    if (email == "") {
+        printError("emailErr", "Введите Email");
+    } else {
+        let regex = /^\S+@\S+\.\S+$/;
+        if (regex.test(email) === false) {
+            printError("emailErr", "Введите Email корректно");
+        } else {
+            printError("emailErr", "");
+            emailErr = false;
+        }
+    }
+
+    if (mobile == "") {
+        printError("mobileErr", "Введите номер телефона");
+    } else {
+        let regex = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+        if (regex.test(mobile) === false) {
+            printError("mobileErr", "Введите номер телефона корректно");
+        } else {
+            printError("mobileErr", "");
+            mobileErr = false;
+        }
+    }
+
+    if (country == "Select") {
+        printError("countryErr", "Выберите вашу страну");
+    } else {
+        printError("countryErr", "");
+        countryErr = false;
+    }
+
+    if (gender == "") {
+        printError("genderErr", "Выберите ваш пол");
+    } else {
+        printError("genderErr", "");
+        genderErr = false;
+    }
+
+    if ((nameErr || emailErr || mobileErr || countryErr || genderErr) == true) {
+        return false;
+    } else {
+        let dataPreview = "Ваша заявка принята: \n" +
+            "Имя: " + name + "\n" +
+            "Email: " + email + "\n" +
+            "Телефон: " + mobile + "\n" +
+            "Страна: " + country + "\n" +
+            "Пол: " + gender + "\n";
+        if (hobbies.length) {
+            dataPreview += "Хобби: " + hobbies.join(", ");
+        } else {
+            dataPreview += "Хобби: нет";
+        }
+        alert(dataPreview);
+        window.location.reload();
+    }
+};
